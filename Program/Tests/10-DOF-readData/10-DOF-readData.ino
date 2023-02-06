812e536b9481f99cb63cb61f911838768db206c7
Ian Anderson <13.ianma@gmail.com>
2023-02-06 08:23:50 -0600

    Create 10-DOF-readData.ino

---
+++ Program/Tests/10-DOF-readData/10-DOF-readData.ino
@@ -0,0 +1,11 @@
+/*
+read all the 10-DOF sensor values and return an array of their values
+*/
+
+float read10DOFData(float arr){ // not sure if this param is right, it is supposed to be an array.
+    // the function is passed an array
+    // read the values from the sensors
+    // then return the array with all the values from the sensors
+
+    return arr;
+}
