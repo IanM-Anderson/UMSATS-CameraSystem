599776d45be10b5941391bacfac0b85e70f5958b
Ian Anderson <13.ianma@gmail.com>
2023-03-02 21:02:39 -0600

    10-DOF read and datalog working

--- Program/Tests/10-DOF-ReadAndDatalog/10-DOF-ReadAndDatalog.ino
+++ Program/Tests/10-DOF-ReadAndDatalog/10-DOF-ReadAndDatalog.ino
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <SPI.h>
+#include <SD.h>
 
 IMU_EN_SENSOR_TYPE enMotionSensorType;
 IMU_ST_ANGLES_DATA stAngles;
@@ -13,6 +15,11 @@ IMU_ST_SENSOR_DATA stMagnRawData;
 float PRESS_DATA=0;
 float TEMP_DATA=0;
 uint8_t u8Buf[3];
+
+const int _MISO = 16;
+const int _MOSI = 19;
+const int _CS = 17;
+const int _SCK = 18;
 
 /**
  * The 2D Array is formatted as:
@@ -29,14 +36,45 @@ void setup(){
     Serial.begin(115200);
     while(init10DOF() == false){
         Serial.println("ERROR init 10-DOF");
-        delay(100);
     }
+    // Ensure the SPI pinout the SD card is connected to is configured properly
+    SPI.setRX(_MISO);
+    SPI.setTX(_MOSI);
+    SPI.setSCK(_SCK);
+
+    // see if the card is present and can be initialized:
+    while(!SD.begin(_CS))
+    {
+      Serial.println("ERROR init 10-DOF");
+    }
+    tone(0, 500);
+    delay(500);
+    noTone(0);
+    delay(500);
+    tone(0, 500);
+    delay(500);
+    noTone(0); 
+}
+
+bool init10DOF()
+{
+    imuInit(&enMotionSensorType);
+ 
+	if(IMU_EN_SENSOR_TYPE_MPU9250 == enMotionSensorType)
+	{
+		Serial.printf("Motion sersor is MPU9250\n" );
+	}
+	if (!LPS22HB_INIT()){
+		Serial.printf("LPS22HB Init Error\n");
+		return false;
+	}
+  return true;
 }
 
 void loop(){
     read10DOFData();
     datalog10DOF(data1);
-    delay(100);
+    delay(200);
 }
 
 void read10DOFData(){
@@ -44,10 +82,18 @@ void read10DOFData(){
     //anglesPiYaRo, pressure, tempertaure, accelerationXYZ, gyroscopeXYZ, magneticXYZ
 
     LPS22HB_START_ONESHOT();
-    if((I2C_readByte(LPS_STATUS)&0x01)==0x01)   //a new pressure data is generated
-    @ -51,12 +59,20 @@ void loop() {
-        u8Buf[1]=I2C_readByte(LPS_TEMP_OUT_H);
-        TEMP_DATA=(float)((u8Buf[1]<<8)+u8Buf[0])/100.0f;
+    if ((I2C_readByte(LPS_STATUS) & 0x01) == 0x01) // a new pressure data is generated
+    {
+        u8Buf[0] = I2C_readByte(LPS_PRESS_OUT_XL);
+        u8Buf[1] = I2C_readByte(LPS_PRESS_OUT_L);
+        u8Buf[2] = I2C_readByte(LPS_PRESS_OUT_H);
+        PRESS_DATA = (float)((u8Buf[2] << 16) + (u8Buf[1] << 8) + u8Buf[0]) / 4096.0f;
+    }
+    if ((I2C_readByte(LPS_STATUS) & 0x02) == 0x02) // a new pressure data is generated
+    {
+        u8Buf[0] = I2C_readByte(LPS_TEMP_OUT_L);
+        u8Buf[1] = I2C_readByte(LPS_TEMP_OUT_H);
+        TEMP_DATA = (float)((u8Buf[1] << 8) + u8Buf[0]) / 100.0f;
     }
 
     imuDataGet( &stAngles, &stGyroRawData, &stAccelRawData, &stMagnRawData);
@@ -69,10 +115,10 @@ void read10DOFData(){
     data1[4][2] = stGyroRawData.s16Z;    
     data1[5][0] = stMagnRawData.s16X;
     data1[5][1] = stMagnRawData.s16Y;
-    data1[5][2] = stMagnRawData.s16Z;    
+    data1[5][2] = stMagnRawData.s16Z;   
 }
 
-void datalog10DOF(float data[][]){
+void datalog10DOF(float data[5][3]){
     // Store all the data in the SD card
     // make a string for assembling the data to log:
     String dataString = "";
